0f7c0c13b944b66e0c0e799c2c4467f17667bcaa
Daviluca05 <125285845+Daviluca05@users.noreply.github.com>
2025-06-17 23:36:46 -0300

    js tela de login

--- autent/script.js
+++ autent/script.js
@@ -1,1 +1,11 @@
-console.log("Página carregada");
+function login() {
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  if (email === 'admin@teste.com' && senha === '123456') {
+    const token = 'secrettoken123';
+    window.location.href = 'http://localhost:8050?token=' + token;
+  } else {
+    document.getElementById('erro').textContent = 'Credenciais inválidas.';
+  }
+}
